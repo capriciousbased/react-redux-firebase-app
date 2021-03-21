279f4144140da4ece83fc6008f68bcc0e180c2e1
capriciousbased <mdanisulhoquekhan@gmail.com>
2021-03-21 11:12:11 +0100

    navbar created

--- src/App.js
+++ src/App.js
@@ -1,13 +1,13 @@
 import { BrowserRouter } from "react-router-dom";
+import Navbar from "./components/layout/Navbar";
 
 function App() {
   return (
     <BrowserRouter>
       <div className="App">
-      <h2>Capricious plan</h2>
-    </div>
+        <Navbar />
+      </div>
     </BrowserRouter>
-    
   );
 }
 
